61970611d808b977c8b60d4eb01892eedcc5efff
Woongrae Cho <woonglae@gmail.com>
2019-11-04 13:28:09 -0500

    Changed the object which is URL in gltf file

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('./simple.json');
+let jsonFile = JSON.parse(rawdata);
+
+let rawgltf = fs.readFileSync('./OutDoor_Meetup.gltf');
+let gltfFile = JSON.parse(rawgltf);
+
+
+var name = jsonFile["name"]
+
+if (gltfFile) {
+    let nameOfTwitch = gltfFile["nodes"][501]["extensions"]["MOZ_hubs_components"]["video"]["src"];
+    // console.log(nameOfTwitch);
+    gltfFile["nodes"][501]["extensions"]["MOZ_hubs_components"]["video"]["src"] = name;
+    // console.log(gltfFile["nodes"][501]["extensions"]["MOZ_hubs_components"]["video"]["src"]);
+}
+
+fs.writeFileSync('file.gltf', JSON.stringify(gltfFile));
+
+
+let rawfile = fs.readFileSync('./file.gltf');
+let changedFile = JSON.parse(rawfile);
+// console.log(changedFile); 
+
+let changedSRC = changedFile["nodes"][501]["extensions"]["MOZ_hubs_components"]["video"]["src"];
+console.log(changedSRC);
