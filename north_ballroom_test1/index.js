105fac891a94c6bc2396732ec7644d3f22b01218
Woongrae Cho <woonglae@gmail.com>
2019-11-11 23:39:33 -0500

    test to export glb file

--- north_ballroom_test1/index.js
+++ north_ballroom_test1/index.js
@@ -1,4 +1,7 @@
-import { ConvertGLBtoGltf, ConvertGltfToGLB, ConvertToGLB} from 'gltf-import-export';
+// node 12.x.x version doesn't support import 
+// import { ConvertGLBtoGltf, ConvertGltfToGLB, ConvertToGLB} from 'gltf-import-export';
+const gltf_import_export = require('gltf-import-export');
+
 const fs = require('fs');
 
 // read JSON file which have url we want to change
@@ -13,36 +16,44 @@ if (!jsonFile["url"]) {
     var urlFromJSON = jsonFile["url"];
     console.log("url is " + urlFromJSON);
 }
+
 const inputGlb = "North_Ballroom.glb";
 const extractedGltfFilename = "newfile.gltf";
 
-ConvertGLBtoGltf(inputGlb,extractedGltfFilename);
+gltf_import_export.ConvertGLBtoGltf(inputGlb,extractedGltfFilename);
 
-// // read GLB file 
-// var rawGLTF = fs.readFileSync('./North Ballroom.gltf');
-// var gltfFile = JSON.parse(rawGLTF);
-// if (!gltfFile) {
-//     console.log("Cannot read gltf file");
-// }
+// read GLB file 
+var rawGLTF = fs.readFileSync(extractedGltfFilename, 'utf8');
+var gltfFile = JSON.parse(rawGLTF);
+if (!gltfFile) {
+    console.log("Cannot read gltf file");
+}
 
 
-// if (gltfFile) {
-//     var nodes = gltfFile["nodes"];
-//     for (var k in nodes) {
-//         // console.log(nodes[k]);
+if (gltfFile) {
+    var nodes = gltfFile["nodes"];
+    for (var k in nodes) {
+        // console.log(nodes[k]);
         
-//         if (nodes[k]["name"]=="Twitch Stream") {
-//             var temp = nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"];
-//             nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"] = jsonFile["url"];
-//             console.log("url has changes from " +temp + " to " + jsonFile["url"]);
-//             break;
-//         }
-//     }
-// } else {
-//     console.log("GLTF file has not been read");
-// }
+        if (nodes[k]["name"]=="Twitch Stream") {
+            var temp = nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"];
+            nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"] = jsonFile["url"];
+            console.log("url has changes from " +temp + " to " + jsonFile["url"]);
+            break;
+        }
+    }
+} else {
+    console.log("GLTF file has not been read");
+}
 
-// fs.writeFileSync('modified.gltf', JSON.stringify(gltfFile));
+const outputGltfName = "modified.gltf";
+const outputGlbName = "modified.gltb";
+
+// fs.writeFileSync(outputGltfName, JSON.stringify(gltfFile));
+
+gltf_import_export.ConvertToGLB(gltfFile, extractedGltfFilename, outputGlbName);
+
+
 
 
 
