2cf78ba25f4d60475e6162b7998c9093d4ec591f
Woongrae Cho <woonglae@gmail.com>
2019-11-11 22:46:14 -0500

    install gltf-import-export through npm

--- north_ballroom_test1/index.js
+++ north_ballroom_test1/index.js
@@ -1,4 +1,4 @@
-import {ConvertGLBtoGltf, ConvertGltfToGLB, ConvertToGLB} from 'gltf-import-export';
+import { ConvertGLBtoGltf, ConvertGltfToGLB, ConvertToGLB} from 'gltf-import-export';
 const fs = require('fs');
 
 // read JSON file which have url we want to change
@@ -12,34 +12,37 @@ if (!jsonFile["url"]) {
 } else {
     var urlFromJSON = jsonFile["url"];
     console.log("url is " + urlFromJSON);
-    
 }
+const inputGlb = "North_Ballroom.glb";
+const extractedGltfFilename = "newfile.gltf";
 
-// read GLB file 
-var rawGLTF = fs.readFileSync('./North Ballroom.gltf');
-var gltfFile = JSON.parse(rawGLTF);
-if (!gltfFile) {
-    console.log("Cannot read gltf file");
-}
+ConvertGLBtoGltf(inputGlb,extractedGltfFilename);
+
+// // read GLB file 
+// var rawGLTF = fs.readFileSync('./North Ballroom.gltf');
+// var gltfFile = JSON.parse(rawGLTF);
+// if (!gltfFile) {
+//     console.log("Cannot read gltf file");
+// }
 
 
-if (gltfFile) {
-    var nodes = gltfFile["nodes"];
-    for (var k in nodes) {
-        // console.log(nodes[k]);
+// if (gltfFile) {
+//     var nodes = gltfFile["nodes"];
+//     for (var k in nodes) {
+//         // console.log(nodes[k]);
         
-        if (nodes[k]["name"]=="Twitch Stream") {
-            var temp = nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"];
-            nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"] = jsonFile["url"];
-            console.log("url has changes from " +temp + " to " + jsonFile["url"]);
-            break;
-        }
-    }
-} else {
-    console.log("GLTF file has not been read");
-}
+//         if (nodes[k]["name"]=="Twitch Stream") {
+//             var temp = nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"];
+//             nodes[k]["extensions"]["MOZ_hubs_components"]["video"]["src"] = jsonFile["url"];
+//             console.log("url has changes from " +temp + " to " + jsonFile["url"]);
+//             break;
+//         }
+//     }
+// } else {
+//     console.log("GLTF file has not been read");
+// }
 
-fs.writeFileSync('modified.gltf', JSON.stringify(gltfFile));
+// fs.writeFileSync('modified.gltf', JSON.stringify(gltfFile));
 
 
 
